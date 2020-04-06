38aee7cb18a4dad0cb6b80e2a4751f2592b9a08d
camilamdaniels <camila.daniels926@gmail.com>
2020-04-06 17:34:13 -0400

    Updates

---
+++ precache-manifest.dd500a0bf63db497960a4369f344286e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeea1ed4e35802c2b7aa7567bd12cbfc",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "18e0ccc72b652d2af49d",
+    "url": "/gameoflife/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "bd7c02f53aeb63842ac1",
+    "url": "/gameoflife/static/css/main.2281bced.chunk.css"
+  },
+  {
+    "revision": "18e0ccc72b652d2af49d",
+    "url": "/gameoflife/static/js/2.656c0644.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gameoflife/static/js/2.656c0644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7c02f53aeb63842ac1",
+    "url": "/gameoflife/static/js/main.1a9a224a.chunk.js"
+  },
+  {
+    "revision": "b482767dd1b7b79b50de",
+    "url": "/gameoflife/static/js/runtime-main.d8e70488.js"
+  }
+]);
